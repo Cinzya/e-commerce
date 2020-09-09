8cf223d476faeb9969e5f64ff5ce8d267f0e091a
Cinzya <cynti-ebert@web.de>
2020-09-09 02:58:29 +0200

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-route-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
